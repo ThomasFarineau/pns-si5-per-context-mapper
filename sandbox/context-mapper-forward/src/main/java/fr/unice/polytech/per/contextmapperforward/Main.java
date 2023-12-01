c64d638a3a25e7e1ce589ea05d7f3b076f9b5c15
Léo K <leo.kitabdjian@etu.unice.fr>
2023-12-01 15:44:37 +0100

    generate images for every cml model #7 #8

--- sandbox/context-mapper-forward/src/main/java/fr/unice/polytech/per/contextmapperforward/Main.java
+++ sandbox/context-mapper-forward/src/main/java/fr/unice/polytech/per/contextmapperforward/Main.java
@@ -5,22 +5,35 @@ import org.contextmapper.dsl.generator.ContextMapGenerator;
 import org.contextmapper.dsl.standalone.ContextMapperStandaloneSetup;
 import org.contextmapper.dsl.standalone.StandaloneContextMapperAPI;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
         // get resources path
-        String cmlFilePath = getResoucesPath("models/al.cml");
-        System.out.println(cmlFilePath);
+        File folder = new File(getResourcesPath("models"));
+        for (File file : folder.listFiles()) {
+            // read the cml file and create the model
+            System.out.println(file.getPath());
+            StandaloneContextMapperAPI contextMapperAPI = ContextMapperStandaloneSetup.getStandaloneAPI();
+            CMLResource cmlResource = contextMapperAPI.loadCML(getResourcesPath("models/" + file.getName()));
 
-        // read the cml file and create the model
-        StandaloneContextMapperAPI contextMapperAPI = ContextMapperStandaloneSetup.getStandaloneAPI();
-        CMLResource cmlResource = contextMapperAPI.loadCML(cmlFilePath);
-
-        ContextMapGenerator contextMapGenerator = new ContextMapGenerator();
-        contextMapperAPI.callGenerator(cmlResource, contextMapGenerator);
+            ContextMapGenerator contextMapGenerator = new ContextMapGenerator();
+            contextMapperAPI.callGenerator(cmlResource, contextMapGenerator);
+        }
     }
 
 
-    private static String getResoucesPath(String fileName) {
+    private static String getResourcesPath(String fileName) {
         return Main.class.getClassLoader().getResource(fileName).getPath();
+    }
+
+    public static void listFilesForFolder(final File folder) {
+        for (final File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                listFilesForFolder(fileEntry);
+            } else {
+                System.out.println(fileEntry.getPath());
+            }
+        }
     }
 }
