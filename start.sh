4ad8ebf4a5e36f61bbc84fc4c4498e7482f17c4e
Léo K <leo.kitabdjian@etu.unice.fr>
2024-02-12 15:55:27 +0100

    edit script to be able to pass a link

--- start.sh
+++ start.sh
@@ -4,7 +4,13 @@ cd discovery
 
 npm run build
 
-npm run exec:windows -- -sn
+if [ $# -eq 0 ]; then
+    echo "No repo provided. Running local."
+    npm run exec:windows -- -sn
+else
+    echo "Repo is provided: $1. Using it."
+    npm run exec:windows -- --repo=$1 -sn
+fi
 
 cd ..
 
